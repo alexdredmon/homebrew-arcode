4793f244d89737fba2e66eebf45edebefdf78fef
Alexandria Redmon <alexandriadredmon@gmail.com>
2024-06-07 21:06:35 -0400

    Bumping version

--- Formula/arcode.rb
+++ Formula/arcode.rb
@@ -2,7 +2,7 @@ class Arcode < Formula
   desc "Arcode: AI driven development tool"
   homepage "https://github.com/alexdredmon/arcode"
   url "https://github.com/alexdredmon/arcode/releases/download/v1.1.0/arcode"
-  version "1.1.0"
+  version "1.1.1"
   sha256 "2b59715126f3b7db1477f4d5a1ff3aa0d58a2a1e824c1ccf7352fa29e4a6ff87"
 
   bottle :unneeded
