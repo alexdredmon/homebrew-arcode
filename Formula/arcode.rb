54e0e4b5f975675598efb15343a5537d1588065f
Alexandria Redmon <alexandriadredmon@gmail.com>
2024-06-07 21:05:15 -0400

    Nixing using nounzip

--- Formula/arcode.rb
+++ Formula/arcode.rb
@@ -1,7 +1,7 @@
 class Arcode < Formula
   desc "Arcode: AI driven development tool"
   homepage "https://github.com/alexdredmon/arcode"
-  url "https://github.com/alexdredmon/arcode/releases/download/v1.1.0/arcode", :using => :nounzip
+  url "https://github.com/alexdredmon/arcode/releases/download/v1.1.0/arcode"
   version "1.1.0"
   sha256 "2b59715126f3b7db1477f4d5a1ff3aa0d58a2a1e824c1ccf7352fa29e4a6ff87"
 
