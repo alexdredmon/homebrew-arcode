59b8107f16fa38fb5249636a6df8fab56c70fc32
Alexandria Redmon <alexandriadredmon@gmail.com>
2024-06-10 00:55:51 -0400

    v1.7.0

--- Formula/arcode.rb
+++ Formula/arcode.rb
@@ -1,9 +1,9 @@
 class Arcode < Formula
   desc "Arcode: AI driven development tool"
   homepage "https://github.com/alexdredmon/arcode"
-  url "https://github.com/alexdredmon/arcode/releases/download/v1.6.1/arcode"
-  version "1.6.1"
-  sha256 "4f84cc3214af67f06d63c5d09bda6bd109d27d9793ec1c4121de1b710ed522df"
+  url "https://github.com/alexdredmon/arcode/releases/download/v1.7.0/arcode"
+  version "1.7.0"
+  sha256 "425472f8fe9f371570606e047bc1d69501dfcdd2bc6a2a8de3a1c33fe76398a8"
 
   def install
     bin.install "arcode"
